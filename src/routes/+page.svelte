64b64947f0735235e0fff0295320fb0f690bfb49
yikuansun <yikuansun2015@gmail.com>
2025-11-24 09:39:48 -0500

    add pixijs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { Application, Assets, Container, Sprite } from "pixi.js";
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        // Create a new application
+        const app = new Application();
+
+        // Initialize the application
+        await app.init({ background: '#1099bb', width: 800, height: 600 });
+
+        // Append the application canvas to the document body
+        document.body.appendChild(app.canvas);
+
+        // Create and add a container to the stage
+        const container = new Container();
+
+        app.stage.addChild(container);
+
+        // Load the bunny texture
+        const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
+
+        // Create a 5x5 grid of bunnies in the container
+        for (let i = 0; i < 25; i++) {
+            const bunny = new Sprite(texture);
+
+            bunny.x = (i % 5) * 40;
+            bunny.y = Math.floor(i / 5) * 40;
+            container.addChild(bunny);
+        }
+
+        // Move the container to the center
+        container.x = app.screen.width / 2;
+        container.y = app.screen.height / 2;
+
+        // Center the bunny sprites in local container coordinates
+        container.pivot.x = container.width / 2;
+        container.pivot.y = container.height / 2;
+
+        // Listen for animate update
+        app.ticker.add((time) => {
+            // Continuously rotate the container!
+            // * use delta to create frame-independent transform *
+            container.rotation -= 0.01 * time.deltaTime;
+        });
+    });
+</script>
